dbd5e1fd49d106991e97c96de64bde8400df848b
Michał Szymanski <michal.t.szymanski@gmail.com>
2012-01-07 12:17:45 +0100

    Updated model class, updated zdjecia page

--- models/location.rb
+++ models/location.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'datamapper'
 require 'dm-mysql-adapter'
 
@@ -16,7 +17,16 @@ class Location
 
   property :id,          Serial
   property :name,        String 
+  property :country,        String 
   property :description, String 
   property :latitude,    Float
   property :longitude,  Float
+
+  def image_dir
+    name.downcase.gsub(/ż/, 'z').gsub(/\s/, '-')
+  end
+
+  def images
+    Dir.entries(File.dirname(__FILE__) + "/../public/images/gallery/" + self.image_dir).select {|file_name| file_name =~ /\.jpg/ }
+  end
 end
